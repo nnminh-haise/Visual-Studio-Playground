1633e00b9e51f23dc2a37575c7386180e7d3520d
nnminh-haise <nnminh.sam.1803@gmail.com>
2023-05-16 17:39:39 +0700

    update

--- Playground/Playground.cpp
+++ Playground/Playground.cpp
@@ -1,93 +1,56 @@
+#include <fstream>
 #include "Student.h"
 #include "Date.h"
 #include "DataStructures/HashMap.h"
 #include "DataStructures/DynamicArray.h"
 #include "DataStructures/LinearList.h"
 #include "DataStructures/LinkedList.h"
-#include "DataStructures/DoubleLinkedList.h"
 #include "DataStructures/Stack.h"
 #include "DataStructures/AVL_Tree.h"
-#include <fstream>
+#include "DataStructures/DoubleLinkedList.h"
 #include <ctime>
 #include <random>
 #include <format>
 
 struct Data {
-	int a;
-	int b;
+	int a_;
+	int b_;
 
-	Data() {
-		this->a = 0;
-		this->b = 0;
-	}
+	Data() : a_(0), b_(0) {}
 
-	Data(int a, int b) {
-		this->a = a;
-		this->b = b;
-	}
+	Data(int a, int b) : a_(a), b_(b) {}
 
-	friend std::ostream& operator<< (std::ostream& os, const Data& data) {
-		os << std::format("a = {}, b = {}", data.a, data.b);
+	friend std::ostream& operator<< (std::ostream& os, const Data& data)
+	{
+		os << std::format("({}, {})", data.a_, data.b_);
 		return os;
 	}
 };
 
-void InOrderTraversal(AVL_Tree<int>::Node* node)
-{
-	if (node != nullptr)
-	{
-		InOrderTraversal(node->left_);
-		std::cout << node->key_ << " ";
-		InOrderTraversal(node->right_);
-	}
-}
+struct Object {
+	DoubleLinkedList<Data> lst_;
+};
 
-int main() {
+int main()
+{
 	srand(time(NULL));
 
-	AVL_Tree<int, int> tree;
+	Object obj;
 
-	int vals[10]{};
-	int keys[10]{};
-	for (int i = 0; i < 10; ++i)
 	{
-		vals[i] = rand() % 100 + 1;
-		keys[i] = rand() % 1000 + 1;
-		std::cout << "Inserting node with key = " << keys[i] << ", val = " << vals[i] << "\n";
-		tree.Insert(keys[i], vals[i]);
+		DoubleLinkedList<Data> list;
+		for (int i = 0; i < 10; ++i)
+		{
+			list.PushBack(Data(i + 1, rand() % 100 + 1));
+		}
+		obj.lst_ = list;
 	}
 
-	std::cout << "tree: ";
-	InOrderTraversal(tree.GetRoot());
-	std::cout << "\n";
-
-	tree.Remove(keys[2]);
-
-	std::cout << "deleteing key = " << keys[2] << "\n";
-	std::cout << "root key = " << tree.GetRoot()->key_ << "\n";
-
-	std::cout << "tree after delete: ";
-	InOrderTraversal(tree.GetRoot());
-	std::cout << "\n";
-
-	std::cout << "search for key[4]: " << tree.Search(keys[4])->key_ << "\n";
-	try {
-		std::cout << "bug: " << tree[keys[2]].key_ << "\n";
-	}
-	catch (const std::exception& ex) {
-		std::cout << ex.what() << "\n";
-	}
-	//std::cout << "search for key[2]: " << tree.Search(keys[2])->info_ << "\n";
-
-	std::cout << "tree size: " << tree.Size() << "\n";
-	LinearList<AVL_Tree<int, int>::Node*> ptrArr(tree.Size());
-	tree.CastToLinearList(ptrArr);
-	int sz = ptrArr.Size();
-	for (int i = 0; i < sz; ++i)
+	for (auto p = obj.lst_.Begin(); p != nullptr; p = p->right_)
 	{
-		std::cout << ptrArr[i]->key_ << " ";
+		std::cout << p->info_ << "; ";
 	}
-	std::cout << "\b\n";
+	std::cout << "\b\b\n";
 
 	return 0;
 }
