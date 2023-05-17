f2e1ef62b4e89de681b12a5a981b31c737ff98bb
nnminh-haise <nnminh.sam.1803@gmail.com>
2023-05-17 12:57:28 +0700

    update

--- Playground/Playground.cpp
+++ Playground/Playground.cpp
@@ -5,11 +5,7 @@
 #include "DataStructures/LinkedList.h"
 #include "DataStructures/Stack.h"
 
-#define TESTING_TYPE Stack<int>
-
-struct Object {
-	TESTING_TYPE list_;
-};
+#define TESTING_TYPE IntLinearList<int>
 
 template<typename T>
 class IntLinearList : public LinearList<T>
@@ -67,60 +63,64 @@ void IntLinearList<T>::PushOrder(const T& value)
 }
 
 
+struct Object {
+	IntLinearList<int> list_;
+};
+
 int main()
 {
-	//Object obj;
+	Object obj;
 
-	//{
-	//	TESTING_TYPE list;
-	//	for (int i = 0; i < 10; ++i)
-	//	{
-	//		list.Push(i + 1);
-	//	}
-	//	obj.list_ = list;
-	//}
-
-	//obj.list_.Print();
-
-	//TESTING_TYPE ls1;
-	//for (int i = 20; i <= 40; ++i)
-	//{
-	//	ls1.Push(i);
-	//}
-
-	//ls1.Print();
-
-	//TESTING_TYPE ls2 = ls1;
-
-	//ls2.Print();
-
-	IntLinearList<int> ls;
-	ls.PushBack(2);
-	ls.PushBack(4);
-	ls.PushBack(6);
-	ls.PushBack(8);
-	ls.PushBack(10);
-	ls.PushBack(12);
-	ls.PushBack(14);
-	ls.PushBack(16);
-	ls.PushBack(18);
-	ls.PushBack(20);
-
-	std::cout << ls.Size() << "\n";
-
-	for (int i = 0; i < ls.Size(); ++i)
 	{
-		std::cout << ls[i] << " ";
+		TESTING_TYPE list;
+		for (int i = 0; i < 10; ++i)
+		{
+			list.PushBack(i + 1);
+		}
+		obj.list_ = list;
 	}
-	std::cout << "\n";
 
-	ls.PushOrder(13);
+	for (int i = 0; i < obj.list_.Size(); ++i) std::cout << obj.list_[i] << " "; std::cout << "\b\n";
 
-	for (int i = 0; i < ls.Size(); ++i)
+	TESTING_TYPE ls1;
+	for (int i = 20; i <= 40; ++i)
 	{
-		std::cout << ls[i] << " ";
+		ls1.PushBack(i);
 	}
-	std::cout << "\n";
+
+	for (int i = 0; i < ls1.Size(); ++i) std::cout << ls1[i] << " "; std::cout << "\b\n";
+
+	TESTING_TYPE ls2 = ls1;
+
+	for (int i = 0; i < ls2.Size(); ++i) std::cout << ls2[i] << " "; std::cout << "\b\n";
+
+	//IntLinearList<int> ls;
+	//ls.PushBack(2);
+	//ls.PushBack(4);
+	//ls.PushBack(6);
+	//ls.PushBack(8);
+	//ls.PushBack(10);
+	//ls.PushBack(12);
+	//ls.PushBack(14);
+	//ls.PushBack(16);
+	//ls.PushBack(18);
+	//ls.PushBack(20);
+
+	//std::cout << ls.Size() << "\n";
+
+	//for (int i = 0; i < ls.Size(); ++i)
+	//{
+	//	std::cout << ls[i] << " ";
+	//}
+	//std::cout << "\n";
+
+	//ls.PushOrder(13);
+
+	//for (int i = 0; i < ls.Size(); ++i)
+	//{
+	//	std::cout << ls[i] << " ";
+	//}
+	//std::cout << "\n";
 
 	return 0;
 }
